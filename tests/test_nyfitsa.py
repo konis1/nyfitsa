a248874eefd0ca0460b4c45800e35583e6c54bd2
Anthony Christodoulou <anthony@situation.sh>
2024-10-29 16:04:35 +0100

    added test error calculate stats

--- tests/test_nyfitsa.py
+++ tests/test_nyfitsa.py
@@ -3,17 +3,18 @@ from typing import Dict
 from unittest.mock import MagicMock
 
 from src.nyfitsa.nyfitsa import fetch_headers
+from src.nyfitsa.nyfitsa import SiteInfos, Results, ErrorCode
 
 
-class test_FetchHeaders():
+class Test_FetchHeaders():
     def test_fetch_headers_all_present(self):
         mock_response = MagicMock()
         mock_response.headers = {
             "server": "Nginx",
-            "x_frame_options": "DENY",
-            "x_content_type_options": "nosniff",
-            "referrer_policy": "no-referrer",
-            "xss_protection": "1; mode=block",
+            "X-Frame-Options": "DENY",
+            "X-Content-Type-Options": "nosniff",
+            "Referrer-Policy": "no-referrer",
+            "X-XSS-Protection": "1; mode=block",
 
         }
 
@@ -34,7 +35,7 @@ class test_FetchHeaders():
         mock_response = MagicMock()
         mock_response.headers = {
             "server": "Nginx",
-            "x_frame_options": "DENY",
+            "X-Frame-Options": "DENY",
 
         }
 
@@ -67,6 +68,102 @@ class test_FetchHeaders():
         result: Dict[str, str] = fetch_headers(mock_response)
 
         assert result == expected_results
+
+
+class TestResults():
+
+    mock_response = MagicMock()
+    mock_response.headers = {
+        "Server": "nginx",
+        "X-Frame-Options": "test",
+        "X-Content-Type-Options": "test",
+        "Referrer-Policy": "test",
+        "X-XSS-Protection": "test",
+    }
+
+    google_site_infos = SiteInfos(
+        url="http://www.google.com",
+        server="nginx",
+        x_frame_options="test",
+        x_content_type_options="test",
+        referrer_policy="test",
+        xss_protection="test",
+        _response=mock_response,
+    )
+
+    wikipedia_site_infos = SiteInfos(
+        url="http://www.wikipedia.com",
+        server="nginx",
+        x_frame_options="test",
+        x_content_type_options="test",
+        referrer_policy="test",
+        xss_protection="test",
+        _response=mock_response,
+    )
+
+    def test_calculate_stats_valid(self):
+
+        expected_results: Dict[str, float] = {
+            "nginx": 100.0,
+        }
+
+        results: Results = Results(site_infos=[
+            self.google_site_infos,
+            self.wikipedia_site_infos
+            ])
+        assert results.stats_server() == expected_results
+        assert results.stats_x_frames_options() == {"test": 100.0}
+        assert results.stats_x_content_type_options() == {"test": 100.0}
+        assert results.stats_referrer_policy() == {"test": 100.0}
+        assert results.stats_xss_protection() == {"test": 100.0}
+
+    def test_calculate_stats_timeout_error(self):
+        mock_response = MagicMock()
+        mock_response.headers = {
+            "Server": None,
+            "X-Frame-Options": None,
+            "X-Content-Type-Options": None,
+            "Referrer-Policy": None,
+            "X-XSS-Protection": None,
+            "err_code": "timeout"
+        }
+
+        google_site_infos = SiteInfos(
+            url="http://www.google.com",
+            server=None,
+            x_frame_options=None,
+            x_content_type_options=None,
+            referrer_policy=None,
+            xss_protection=None,
+            err_code=ErrorCode.TIMEOUT,
+            _response=mock_response,
+        )
+
+        wikipedia_site_infos = SiteInfos(
+            url="http://www.wikipedia.com",
+            server=None,
+            x_frame_options=None,
+            x_content_type_options=None,
+            referrer_policy=None,
+            xss_protection=None,
+            err_code=ErrorCode.TIMEOUT,
+            _response=mock_response,
+        )
+
+        expected_results: Dict[str, float] = {
+            "timeout": 100.0,
+        }
+
+        results: Results = Results(site_infos=[
+            google_site_infos,
+            wikipedia_site_infos
+            ])
+        assert results.stats_server() == expected_results
+        assert results.stats_x_frames_options() == expected_results
+        assert results.stats_x_content_type_options() == expected_results
+        assert results.stats_referrer_policy() == expected_results
+        assert results.stats_xss_protection() == expected_results
+
 
 # def test_stats_server_valid():
 #     urls: List[str] = ["http://www.google.com", "https://www.wikipedia.com"]
